72a9aecb625b8b15f017ad849e9cfa4d4e4068d7
Emreozgun <emreozgun59@gmail.com>
2024-04-07 14:01:22 +0300

    seed data added for elasticsearch products index.

--- docker/db-init/entrypoint.sh
+++ docker/db-init/entrypoint.sh
@@ -4,3 +4,39 @@ set -e
 sleep 2
 yarn db:migrate
 yarn db:seed
+
+curl -s "http://elasticsearch:9200"
+
+curl -XDELETE "http://elasticsearch:9200/_all"
+
+curl -XPUT "http://elasticsearch:9200/products"
+
+curl -XPUT "http://elasticsearch:9200/products/_mapping" -H 'Content-Type: application/json' -d'
+{
+  "properties": {
+    "name": {
+      "type": "text",
+      "analyzer": "standard",
+      "fields": {
+        "full_text": {
+          "type": "text",
+          "analyzer": "fulltext_analyzer"
+        }
+      }
+    },
+    "item_id": {
+      "type": "keyword"
+    },
+    "locale": {
+      "type": "keyword"
+    },
+    "click": {
+      "type": "integer"
+    },
+    "purchase": {
+      "type": "integer"
+    }
+  }
+}
+'
+curl -XPOST -H "Content-Type: application/json"  "http://elasticsearch:9200/_bulk" --data-binary  @/app/bulk.json
